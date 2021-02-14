1a32b2ba9328609e02531689f4037f5a195fda5e
sdas <sdas@arguscontrols.com>
2021-02-14 13:37:57 -0800

    Added test for odm manager

--- odm_test.py
+++ odm_test.py
@@ -1,10 +1,17 @@
 from odmmanager import OdmManager
 import asyncio
+import concurrent.futures
+from auth import (hostname, port, token)
+
+async def wrapped_process_image():
+    executor = concurrent.futures.ThreadPoolExecutor()
+    await loop.run_in_executor(executor, om.processImages())
 
 async def run():
-    
+    await wrapped_process_image()
 
 if __name__== "__main__":
     om = OdmManager()
+    om.config("/images", token, hostname, port)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run())
